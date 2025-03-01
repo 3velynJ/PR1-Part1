7c4a3ec9cd277168e12d36e1698975fcbcd18f16
Evelyn <evelynjohns547@gmail.com>
2025-03-01 14:34:23 +0000

    Create Song class

---
+++ src/Song.java
@@ -0,0 +1,15 @@
+public class Song {
+    private String name;
+    private String artist;
+    private int playCount;
+
+    public Song(String name, String artist, int playCount) {
+        this.name = name;
+        this.artist = artist;
+        this.playCount = playCount;
+    }
+
+    public String displaySong() {
+        return this.name + " - " + this.artist + " (" + this.playCount + ")";
+    }
+}
